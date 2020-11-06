6f5118aaa38f7e6aa5ca79414c0524bb98e2f317
itggot-daniel-persson <59963395+itggot-daniel-persson@users.noreply.github.com>
2020-11-06 11:18:36 +0100

    Added Car class (abstract)

---
+++ src/Car.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+
+public abstract class Car
+{
+    private int nrDoors; // number of doors on the car
+    private Color color;
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private String modelName; // The car model name
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+
+    protected abstract double speedFactor();
+
+    protected abstract void incrementSpeed(double amount);
+
+    protected abstract void decrementSpeed(double amount);
+
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+
+
+}
